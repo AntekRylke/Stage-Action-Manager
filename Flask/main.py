b492ce33a2cf78fcac51d4f03c65e62ec8ad840c
AntekRylke <antoni.rylke@stud.vistula.edu.pl>
2022-12-02 17:33:20 +0100

    hh

--- Flask/main.py
+++ Flask/main.py
@@ -90,7 +90,8 @@ def newclient():
          # Utworzenie zmiennej, przechowującej informacje o nowym urządzeniu klienckim
          newclient = []
          newclient.append(str(len(client_data))) # ID
-         newclient.append
+         newclient.append(request.form.get('IP_1') + "." + request.form.get('IP_2') + "." + request.form.get('IP_3') + "." + request.form.get('IP_4'))
+         
          #newclient("client_data.csv", )
          #client_data[]["ID"] = len(client_data)
          #client_data[len(client_data)]["name"] = request.form.get('name')
